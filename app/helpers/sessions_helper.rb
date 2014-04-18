21b48fe8e0ae3ee1fe30ded445fe742031fbd487
Jaron Heard <jaronheard@gmail.com>
2014-04-17 22:07:21 -0700

    finish sign-in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.hash(remember_token))
-    self.current_user
+    self.current_user = user
   end
   
   def current_user
@@ -11,7 +11,17 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: remember_token)
   end
   
+  def current_user=(user)
+      @current_user = user
+    end
+  
   def signed_in?
     !current_user.nil?
+  end
+  
+  def sign_out
+    current_user.update_attribute(:remember_token, User.hash(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 end
